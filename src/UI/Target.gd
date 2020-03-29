2abb61befde865e45ce9112807e730f6f3b38761
Eric Imperio <imperio19@up.edu>
2020-03-29 13:16:06 -0700

    Added a Target for Cheeesecakes to follow
    Use Left Click

---
+++ src/UI/Target.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	connect("body_entered", self, "_on_body_entered")
+	visible = false
+
+func _input(event: InputEvent):
+	if event is InputEventMouseButton and event.get_button_index() == 1:
+		anim_player.play("fade_in")
+		global_position = get_global_mouse_position()
+
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	anim_player.play("fade_out")
