171a2ae633d267b9c1ba6af760cce5758a5ba344
Eric Imperio <imperio19@up.edu>
2020-04-13 18:42:23 -0700

    Enemies now do 1 of 7 movements
    each enemy is assigned a movement forever

--- src/items/ingredient.gd
+++ src/items/ingredient.gd
@@ -7,6 +7,9 @@ func _on_ingredient_body_entered(body):
 	if "goodGuy" in body:
 		player.add_ingredient()
 		self.get_parent().remove_child(self)
+	elif "badGuy" in body:
+		self.get_parent().get_child(body.get_index()).wait = 1
+		self.get_parent().remove_child(self)
 
 func _process(delta):
 	self.rotation += 1 * delta
