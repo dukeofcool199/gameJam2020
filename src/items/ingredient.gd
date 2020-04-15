a3998a7cba13e22f2126e5a30e5e7573193764fc
Jenkin Schibel <jenkin.schibel@protonmail.com>
2020-04-14 23:44:07 -0700

    spawned items are now of three random types

--- src/items/ingredient.gd
+++ src/items/ingredient.gd
@@ -1,7 +1,28 @@
 extends Area2D
 
 onready var player = self.get_parent().find_node("player")
+onready var sprite = $Sprite
+const BUTTER = 0
+const CREAMCHEESE = 1
+const CRACKER = 2
+var type
 
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	type = rng.randi_range(1, 2)
+	print(type)
+	if type == self.BUTTER:
+		sprite.texture = load("res://assets/tempAssets/butter.png")
+		
+	elif type == self.CREAMCHEESE:
+		sprite.texture = load("res://assets/tempAssets/creamCheese.jpg")
+		pass
+	
+	elif type == self.CRACKER:
+		sprite.texture = load("res://assets/tempAssets/grahamCracker.jpg")
+		pass
 
 func _on_ingredient_body_entered(body):
 	if "goodGuy" in body:
